766bf9f05451ca311f7256a7ea17bb1b88022d89
Yihui Xie <xie@yihui.name>
2012-02-16 00:51:00 -0600

    update plugin info

--- animation/syntax.php
+++ animation/syntax.php
@@ -20,10 +20,10 @@ class syntax_plugin_animation extends DokuWiki_Syntax_Plugin {
       return array(
 		   'author' => 'Yihui Xie',
 		   'email'  => 'xie@yihui.name',
-		   'date'   => '2011-01-31',
+		   'date'   => '2012-02-16',
 		   'name'   => 'Animation Plugin',
 		   'desc'   => 'Generate an animation from a sequence of images, e.g. 1.png, 2.png, ...',
-		   'url'    => 'http://animation.yihui.name/wiki:animation_plugin',
+		   'url'    => 'https://github.com/yihui/dokuwiki',
 		   );
     }
     function getType() { return 'substition';}
